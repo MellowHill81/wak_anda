af2c9ccad4cc7de06b4111e983b35438541769bf
MellowHil81 <calvinpeter.k@gmail.com>
2025-02-03 13:24:46 +0300

    innitial commit

---
+++ variable.py
@@ -0,0 +1,22 @@
+#a variable is a container used to store data values
+
+number = 10
+firstname = "Calvin"
+lastname = "Mellow"
+language = "Python"
+a = 10
+b = 15
+
+print(number)
+print(firstname)
+print(lastname)
+print(firstname,lastname)
+print(language, "is a web programming language")
+print(a + b)
+#The sum of 10 and 15 is 25
+
+a = 10
+b= 15
+c = 25
+#The sum of 10 and 15 is 25
+print("The sum of 10 and 15 is 25")
